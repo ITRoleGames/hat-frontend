f93636a1ae65f06d62f7bb19f83b2dd98b980d91
Anton Orlov <orlov88@gmail.com>
2023-01-12 15:32:30 +0300

    HAT-32 Наброски работы с сокетами

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { CompatClient, Stomp } from "@stomp/stompjs";
 import CreateGame from "components/create-game/сreate-game-page.component";
 import CreateWordsPage from "components/create-words-page/create-words-page.component";
 import GameCreatedPage from "components/game-created/game-created-page.component";
@@ -5,12 +6,72 @@ import HomePage from "components/home-page.component";
 import JoinGamePage from "components/join-game/join-game-page.component";
 import PageNotFound from "components/page-not-found.component";
 import PrivateRoute from "components/private-route";
+import WaitingPlayersPage from "components/wait-for-players/waiting-players-page.component";
+import { useState } from "react";
+import { useEffect } from "react";
 import React from "react";
 import { Routes } from "react-router";
 import { BrowserRouter, Route } from "react-router-dom";
+import SockJS from "sockjs-client";
 import "./App.css";
 
+const WS_URL = "ws://localhost:9000/game";
+
 function App() {
+
+    const [stompClient, setStompClient] = useState<CompatClient | undefined>()
+    const [messages, setMessages] = useState<string[]>([])
+    const [disconnectMessages, setDisconnectMessages] = useState<string[]>([])
+
+    function connect() {
+        const socket = new SockJS("http://localhost:9002/api/v1/game/ws"); //todo: why http
+        const stompClient = Stomp.over(socket);
+        stompClient.connect({}, (frame: any) => {
+            console.log("Connected: " + frame);
+            stompClient.subscribe("/topic/game", function (msg) {
+                console.log("message received " + JSON.stringify(msg))
+                showGreeting(JSON.parse(msg.body).content);
+            });
+            // stompClient.subscribe('/topic/chat/disconnect', function (msg) {
+            //     setDisconnectMessages(x => [...x, JSON.parse(msg.body).content]);
+            // });
+        });
+        return stompClient;
+    }
+
+    function disconnect() {
+        if (stompClient) {
+            stompClient.send("/app/chat/disconnect", {}, JSON.stringify({"content": "disconnected"}));
+            stompClient.disconnect();
+        }
+        console.log("Disconnected");
+    }
+
+    function sendMessage(message: string) {
+        if (stompClient) {
+            stompClient.send("/app/chat/message", {}, JSON.stringify({"content": message}));
+        }
+    }
+
+    function showGreeting(message: string) {
+        console.log("called")
+        setMessages(x => [...x, message])
+    }
+
+    function clearDisconnectMessage() {
+        setDisconnectMessages([])
+    }
+
+
+    useEffect(() => {
+        if (!stompClient) {
+            setStompClient(connect())
+        }
+        return () => {
+            disconnect()
+        }
+    }, [])
+
     return (
         <BrowserRouter>
             <Routes>
@@ -19,6 +80,7 @@ function App() {
                 <Route path="/joinGame" element={ <JoinGamePage/> }/>
                 <Route path="/gameCreated" element={ <PrivateRoute component={ GameCreatedPage }/> }/>
                 <Route path="/createWords" element={ <PrivateRoute component={ CreateWordsPage }/> }/>
+                <Route path="/waitingPlayers" element={ <PrivateRoute component={ WaitingPlayersPage }/> }/>
                 <Route path="*" element={ <PageNotFound/> }>
                 </Route>
             </Routes>
