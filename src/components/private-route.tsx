d61fa01c72f8bb89a722c348eed655cbd65f7d64
Anton Orlov <orlov88@gmail.com>
2023-01-17 17:49:51 +0300

    HAT-32 Загрузка данных игры при перезагрузке страницы

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -1,24 +1,35 @@
-import { getCurrentUserAction } from "actions/user.action";
-import { FC } from "react";
-import { ConnectedProps } from "react-redux";
-import { connect } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { RootState } from "reducers/combine";
-import { ThunkDispatch } from "redux-thunk";
-import { isUserLoggedIn } from "service/local-storage";
+import {getCurrentUserAction} from "actions/user.action";
+import {FC} from "react";
+import {connect, ConnectedProps} from "react-redux";
+import {Navigate} from "react-router-dom";
+import {RootState} from "reducers/combine";
+import {ThunkDispatch} from "redux-thunk";
+import {getGameId, isUserLoggedIn} from "service/local-storage";
+import {getGameAction} from "../actions/game.action";
 
 interface PrivateRouteProps {
     component: React.FC;
 }
 
-const PrivateRoute: FC<PrivateRouteProps & ConnectedProps<typeof connector>> = ({
-                                                                                       component: Component,
-                                                                                       userState,
-                                                                                       getUser,
-                                                                                   }) => {
+const PrivateRoute: FC<PrivateRouteProps & ConnectedProps<typeof connector>> = (
+    {
+        component: Component,
+        userState,
+        gameState,
+        getUser,
+        getGame
+    }
+) => {
     if (isUserLoggedIn()) {
         if (userState.user == null && !userState.loading) {
             getUser();
+        }
+
+        if (gameState.game == null && !gameState.loading) {
+            const gameId = getGameId();
+            if (gameId) {
+                getGame(gameId);
+            }
         }
         return <Component/>;
     }
@@ -28,10 +39,12 @@ const PrivateRoute: FC<PrivateRouteProps & ConnectedProps<typeof connector>> = (
 
 const mapStateToProps = (state: RootState) => ({
     userState: state.user,
+    gameState: state.game,
 });
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
     getUser: async () => await dispatch(getCurrentUserAction()),
+    getGame: async (id: string) => await dispatch(getGameAction(id)),
 });
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
