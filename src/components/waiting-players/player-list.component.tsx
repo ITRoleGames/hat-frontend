7dec4a5a08944e3170533a943f5568730f28d130
Anton Orlov <orlov88@gmail.com>
2023-01-19 15:18:32 +0300

    HAT-32 Компонент ожидания пользователей

---
+++ src/components/waiting-players/player-list.component.tsx
@@ -0,0 +1,32 @@
+import {Game} from "model/game.model";
+import {User} from "model/user.model";
+import {PersonCircle} from "react-bootstrap-icons";
+
+type Props = {
+    game: Game,
+    gameUsers: User[]
+};
+
+function PlayersList({game, gameUsers}: Props) {
+
+    const users = game.users.map(u => {
+        const gameUser = gameUsers?.find(gu => gu.id == u);
+        return {id: u, name: gameUser?.name}
+    });
+
+    return (
+        <div className="col-lg-2 col-md-5 mx-auto d-flex flex-column">
+            {
+                users.map(user => {
+                    return <div key={user.id} className="d-flex flex-row justify-content-left mb-2">
+                        <PersonCircle size="50" color="grey"/>
+                        <div className="align-self-center text-start ms-2"> {user.name ? user.name : user.id}</div>
+                    </div>
+                })
+            }
+        </div>
+    );
+
+}
+
+export default PlayersList;
--- src/components/waiting-players/waiting-players-page.component.tsx
+++ src/components/waiting-players/waiting-players-page.component.tsx
@@ -1,18 +1,34 @@
 import Header from "../header/header.component";
 import {useTranslation} from "react-i18next";
+import PlayersList from "./player-list.component";
+import {RootState} from "../../reducers/combine";
+import {connect, ConnectedProps} from "react-redux";
 
-const WaitingPlayersPage = () => {
+const WaitingPlayersPage: React.FC<Props> = ({gameState, gameUsersState}) => {
 
     const {t} = useTranslation();
+    const loading = gameState.loading || gameUsersState.loading
 
     return (
         <>
             <Header/>
             <div className="px-4 text-center">
-                <h1 className="display-3 fw-bold"> {t("waitingPlayers.title")} </h1>
+                <h1 className="display-5 fw-bold">{t("waitingPlayers.title")}</h1>
+                {loading && <>Loading...</>}
+                {!loading && <PlayersList game={gameState.game!!} gameUsers={gameUsersState.users}/>}
             </div>
         </>
     );
 };
 
-export default WaitingPlayersPage
+const mapStateToProps = (state: RootState) => ({
+    gameState: state.game,
+    gameUsersState: state.gameUsers,
+});
+
+
+const connector = connect(mapStateToProps, null);
+
+type Props = ConnectedProps<typeof connector>;
+
+export default connector(WaitingPlayersPage);
