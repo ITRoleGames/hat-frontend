e14530c55841e88ad6df9b0f01e4e6df4ef259f4
Anton Orlov <orlov88@gmail.com>
2023-01-19 15:16:32 +0300

    HAT-32 Слайс gameUsers

--- src/reducers/combine.ts
+++ src/reducers/combine.ts
@@ -1,10 +1,14 @@
 import gameReducer from "reducers/game.reducer";
 import userReducer from "reducers/user.reducer";
 import { combineReducers } from "redux";
+import eventReducer from "./event.reducer";
+import gameUsersSlice from "../slice/game-users.slice";
 
 const reducers = combineReducers({
     user: userReducer,
     game: gameReducer,
+    gameUsers: gameUsersSlice,
+    event: eventReducer,
 });
 
 export default reducers;
---
+++ src/slice/game-users.slice.ts
@@ -0,0 +1,59 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {User} from "../model/user.model";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {AnyAction} from "redux";
+import {UserApi} from "../api/user.api";
+import {AxiosError} from "axios";
+
+const initialState: GameUsersState = {
+    users: [],
+    loading: false,
+    error: null
+};
+
+const gameUsersSlice = createSlice({
+    name: "gameUsers",
+    initialState,
+    reducers: {
+        getGameUsersActionPending(state: GameUsersState) {
+            state.loading = true
+        },
+        getGameUsersActionSuccess: (state: GameUsersState, action: PayloadAction<User[]>) => {
+            state.loading = false
+            state.users = mergeUsers(state.users, action.payload)
+        },
+        getGameUsersActionFailed: (state: GameUsersState, action: PayloadAction<string>) => {
+            state.loading = false
+            state.error = action.payload
+        },
+    }
+})
+
+const mergeUsers = (firstUserList: User[], secondUserList: User[]): User[] => {
+
+    const firstUserListIds = firstUserList.map(u => u.id);
+    return [...firstUserList, ...secondUserList.filter((user) => firstUserListIds.indexOf(user.id) < 0)]
+}
+
+interface GameUsersState {
+    users: User[];
+    loading: boolean;
+    error: string | null;
+}
+
+export const getGameUsersAction = (ids: string[]): ThunkAction<Promise<User[]>, {}, {}, AnyAction> => {
+    return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<User[]> => {
+        return new Promise<User[]>((resolve) => {
+            dispatch(getGameUsersActionPending());
+
+            return UserApi.getUsers(ids).then((users: User[]) => {
+                dispatch(getGameUsersActionSuccess(users));
+                resolve(users);
+            }).catch((error: AxiosError) => dispatch(getGameUsersActionFailed(error.message)));
+        });
+    };
+};
+
+export const {getGameUsersActionPending, getGameUsersActionSuccess, getGameUsersActionFailed} = gameUsersSlice.actions
+
+export default gameUsersSlice.reducer;
