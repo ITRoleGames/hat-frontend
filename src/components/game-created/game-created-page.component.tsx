c3587a1fc5d43a019a79a92a7c0652856adf8579
Anton Orlov <orlov88@gmail.com>
2023-02-22 14:45:35 +0300

    HAT-71 Изменение экрана кода игры
    - Код может быть скопирован в буфер
    - Можно поделитьсяч ссылкой для присоединения и кодом в telegram

--- src/components/game-created/game-created-page.component.tsx
+++ src/components/game-created/game-created-page.component.tsx
@@ -1,31 +1,43 @@
 import Header from "components/header/header.component";
-import { Button } from "react-bootstrap";
-import { useTranslation } from "react-i18next";
-import { ConnectedProps } from "react-redux";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { RootState } from "reducers/combine";
+import {Button} from "react-bootstrap";
+import {useTranslation} from "react-i18next";
+import {connect, ConnectedProps} from "react-redux";
+import {Link} from "react-router-dom";
+import {RootState} from "reducers/combine";
+import InputWithCopyButton from "./input-with-copy-button";
+import Loading from "../common/loading.component";
+import {Telegram} from "react-bootstrap-icons";
 
-const GameCreatedPage: React.FC<GameCreatedProps> = ({ gameState }) => {
+const GameCreatedPage: React.FC<GameCreatedProps> = ({gameState}) => {
 
-    const { game } = gameState;
-    const { t } = useTranslation();
+    const {game, loading} = gameState;
+    const {t} = useTranslation();
+
+    const shareWithTelegram = (gameCode: string) => {
+        const url = `${window.location.origin}/joinGame?code=${gameCode}`
+        const text = `${t("gameCreated.code")}: ${gameCode}`
+        window.location.href = `tg://msg_url?url=${url}&text=${text}`
+    };
 
     return (
         <>
             <Header/>
-            <div className="px-4 text-center">
-                <h1 className="display-3 fw-bold"> { t("gameCreated.title") } </h1>
-                <h2 className="display-6">{ t("gameCreated.code") }</h2>
-                <div className="display-5">{ game?.code }</div>
-
-                <div className="py-5 d-grid gap-5 gap-5 col-lg-6 mx-auto">
-                    <Button variant="success" size="lg" onClick={ () => {
-                        alert("Not impl");
-                    } }> { t("btn.share") } </Button>
-                    <Link to="/addWords" className="btn btn-lg btn-primary"> { t("btn.next") } </Link>
+            {loading && <Loading/>}
+            {(!loading && game) &&
+                <div className="px-4 text-center">
+                    <h1 className="display-3 fw-bold"> {t("gameCreated.title")} </h1>
+                    <div className="col-lg-6 mx-auto">
+                        <InputWithCopyButton value={game.code} label={t("gameCreated.code")}/>
+                    </div>
+                    <div className="py-5 d-grid gap-5 gap-5 col-lg-6 mx-auto">
+                        <Button variant="success" size="lg" onClick={() => shareWithTelegram(game.code)}>
+                            {t("btn.share")} &nbsp;
+                            <Telegram/>
+                        </Button>
+                        <Link to="/addWords" className="btn btn-lg btn-primary"> {t("btn.next")} </Link>
+                    </div>
                 </div>
-            </div>
+            }
         </>
     );
 };
