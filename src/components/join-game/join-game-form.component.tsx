c7603b9a534ec860ecd439d7e65eaa95050fe2c3
Anton Orlov <orlov88@gmail.com>
2023-02-22 14:48:32 +0300

    HAT-71 Автоподстановка кода при присединении к игре

--- src/components/join-game/join-game-form.component.tsx
+++ src/components/join-game/join-game-form.component.tsx
@@ -1,39 +1,44 @@
-import { JoinGameFormData } from "components/join-game/join-game-form-data";
-import { Button } from "react-bootstrap";
-import { useForm } from "react-hook-form";
-import { SubmitHandler } from "react-hook-form";
-import { useTranslation } from "react-i18next";
+import {JoinGameFormData} from "components/join-game/join-game-form-data";
+import {Button} from "react-bootstrap";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useTranslation} from "react-i18next";
+import {useSearchParams} from "react-router-dom";
 
 type Props = {
     onFormSubmit: (data: JoinGameFormData) => void;
     loading: boolean
 };
 
-function JoinGameForm({ onFormSubmit, loading }: Props) {
+function JoinGameForm({onFormSubmit, loading}: Props) {
+
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get("code");
 
     const {
         register,
         handleSubmit,
-        formState: { errors },
-    } = useForm<JoinGameFormData>();
-    const { t } = useTranslation();
+        formState: {errors},
+    } = useForm<JoinGameFormData>({
+        defaultValues: {code: code || ""}
+    })
+    const {t} = useTranslation();
 
     const onSubmit: SubmitHandler<JoinGameFormData> = data => onFormSubmit(data);
 
     return (
         <div className="col-md-6 mx-auto">
-            <form onSubmit={ handleSubmit(onSubmit) }>
-                <label>{ t("joinGame.form.code") }</label>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>{t("joinGame.form.code")}</label>
                 <div className="input-group has-validation">
                     <input
-                        className={ "form-control " + (errors?.code ? "is-invalid" : "") }
-                        { ... register("code", { required: true }) }
+                        className={"form-control " + (errors?.code ? "is-invalid" : "")}
+                        {...register("code", {required: true})}
                     />
-                    { errors?.code?.type === "required" &&
-                        <div className="invalid-feedback">{  t("error.requiredField") }</div> }
+                    {errors?.code?.type === "required" &&
+                        <div className="invalid-feedback">{t("error.requiredField")}</div>}
                 </div>
                 <div className="py-5 d-grid gap-5 mx-auto">
-                    <Button type="submit" variant="primary" size="lg" disabled={loading}> { t("btn.join") } </Button>
+                    <Button type="submit" variant="primary" size="lg" disabled={loading}> {t("btn.join")} </Button>
                 </div>
             </form>
         </div>
