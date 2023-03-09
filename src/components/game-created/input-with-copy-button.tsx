56192f4e879c5e6defece7659ef66832d5ef4a19
Anton Orlov <orlov88@gmail.com>
2023-03-09 18:22:05 +0300

    HAT-71 Фикс копирования кода

--- src/components/game-created/input-with-copy-button.tsx
+++ src/components/game-created/input-with-copy-button.tsx
@@ -6,8 +6,40 @@ function InputWithCopyButton({value, label}: Props) {
 
     const {t} = useTranslation();
 
+    const copyCodeAndNotify = async (text: string) => {
+        await copyToClipboard(text)
+            .then(() => notifyCopied())
+            .catch(_ => notifyError())
+    }
+
     const copyToClipboard = async (text: string) => {
-      await navigator.clipboard.writeText(text).then(() => notifyCopied())
+        return new Promise((resolve, reject) => {
+            if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                const textarea = document.createElement("textarea");
+                textarea.textContent = text;
+                textarea.style.position = "fixed";
+                textarea.style.width = "2em";
+                textarea.style.height = "2em";
+                textarea.style.padding = "0";
+                textarea.style.border = "none";
+                textarea.style.outline = "none";
+                textarea.style.boxShadow = "none";
+                textarea.style.background = "transparent";
+                document.body.appendChild(textarea);
+                textarea.focus();
+                textarea.select();
+                try {
+                    document.execCommand("copy");
+                    document.body.removeChild(textarea);
+                    resolve("");
+                } catch (e) {
+                    document.body.removeChild(textarea);
+                    reject(e);
+                }
+            } else {
+                reject(new Error("None of copying methods are supported by this browser!"));
+            }
+        });
     }
 
     const notifyCopied = () => {
@@ -16,14 +48,30 @@ function InputWithCopyButton({value, label}: Props) {
             type: "info",
             insert: "top",
             container: "top-full",
-            animationIn: [ "animate__animated", "animate__fadeIn" ],
-            animationOut: [ "animate__animated", "animate__fadeOut" ],
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
             dismiss: {
                 duration: 2000,
                 onScreen: true,
             },
         });
     }
+
+    const notifyError = () => {
+        Store.addNotification({
+            message: t("codeCopyError"),
+            type: "danger",
+            insert: "top",
+            container: "top-full",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+                duration: 2000,
+                onScreen: true,
+            },
+        });
+    }
+
     return (
         <div className="col-lg-3 mx-auto">
             <label htmlFor="game-code" className="col-form-label">{label}</label>
@@ -37,7 +85,7 @@ function InputWithCopyButton({value, label}: Props) {
                        disabled={true}
                 />
                 <button className="btn btn-outline-secondary" type="button" id="copy-code"
-                        onClick={() => copyToClipboard(value)}>
+                        onClick={() => copyCodeAndNotify(value)}>
                     <Files/>
                 </button>
             </div>
