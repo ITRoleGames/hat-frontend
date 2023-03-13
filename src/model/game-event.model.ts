12bcfb16f7649d80bf700244bedfc9477b7ee4f4
OrlovAnton88 <orlov88@gmail.com>
2023-03-13 19:14:10 +0300

    HAT-67 - Миграция на rSocket (#8)
    * HAT-67 Подключение RSocket вместо WebSocket

--- src/model/game-event.model.ts
+++ src/model/game-event.model.ts
@@ -3,4 +3,6 @@ export interface GameEvent {
     type: string;
 
     gameId: string
+
+    actorUserId: string
 }
--- src/service/web-socket-middleware.tsx
+++ src/service/web-socket-middleware.tsx
@@ -1,6 +1,21 @@
 import {Middleware, MiddlewareAPI} from "redux"
 import {connectingAction, EventActionType} from "../actions/event.actions";
-import {CompatClient, Stomp} from "@stomp/stompjs";
+import {
+    BufferEncoders,
+    encodeBearerAuthMetadata,
+    encodeCompositeMetadata,
+    encodeRoute,
+    IdentitySerializer,
+    JsonSerializer,
+    MAX_STREAM_ID,
+    MESSAGE_RSOCKET_AUTHENTICATION,
+    MESSAGE_RSOCKET_COMPOSITE_METADATA,
+    MESSAGE_RSOCKET_ROUTING,
+    RSocketClient
+} from "rsocket-core";
+import RSocketWebSocketClient from "rsocket-websocket-client";
+import {ISubscription} from "rsocket-types/ReactiveStreamTypes";
+import {ReactiveSocket} from "rsocket-types";
 import {getGameActionFailed, getGameActionPending, getGameActionSuccess} from "../actions/game.action";
 import {GameApi} from "../api/game.api";
 import {Game} from "../model/game.model";
@@ -16,13 +31,12 @@ import {getAccessToken} from "./local-storage";
 
 const webSocketMiddleware: Middleware = store => {
 
-    let stompClient: CompatClient;
-
     return next => action => {
 
         const connecting = store.getState().eventState.connecting;
         const connected = store.getState().eventState.connected;
         const gameId = store.getState().game.game?.id;
+        const userId = store.getState().user.user?.id;
         if (
             gameId
             && !connected
@@ -31,47 +45,89 @@ const webSocketMiddleware: Middleware = store => {
             && action.type != EventActionType.CONNECTED
         ) {
             store.dispatch(connectingAction())
-            stompClient = connect(gameId, store)
+            connect(gameId, userId, store)
         }
 
         next(action);
     }
 }
 
-const connect = (gameId: string, store: MiddlewareAPI): CompatClient => {
-    const users = store.getState().gameUsers.users;
-
-    let webSocketUrl: string;
+const rsocketUrl = () => {
     if (process.env.REACT_APP_WEBSOCKET_URL) {
-        webSocketUrl = process.env.REACT_APP_WEBSOCKET_URL
+        return process.env.REACT_APP_WEBSOCKET_URL
     } else {
-        const url = new URL("ws", window.location.href);
+        const url = new URL("rsocket", window.location.href);
         url.protocol = url.protocol.replace("http", "ws");
-        webSocketUrl = url.href;
+        return url.href;
+    }
+}
+
+const connect = (gameId: string, userId: string, store: MiddlewareAPI) => {
+
+    const users = store.getState().gameUsers.users;
+    const route = `/games/${gameId}/user/${userId}`;
+    const accessToken = getAccessToken();
+    if (accessToken == null) return
+    const client = new RSocketClient({
+        serializers: {
+            data: JsonSerializer,
+            metadata: IdentitySerializer
+        },
+        setup: {
+            keepAlive: 60000,
+            lifetime: 180000,
+            dataMimeType: "application/json",
+            metadataMimeType: MESSAGE_RSOCKET_COMPOSITE_METADATA.string,
+            payload: {
+                metadata: encodeCompositeMetadata([
+                    [MESSAGE_RSOCKET_ROUTING, encodeRoute(route)],
+                    [MESSAGE_RSOCKET_AUTHENTICATION, encodeBearerAuthMetadata(accessToken)],
+                ])
+            }
+        },
+        transport: new RSocketWebSocketClient({
+            url: rsocketUrl()
+        }, BufferEncoders)
+    });
+
+    const errorHandler = (e: any) => console.error(e);
+
+    const responseHandler = (payload: any) => {
+        const gameEvent = payload.data
+        if (gameEvent.type == "GAME_UPDATED" && gameEvent.actorUserId != userId) {
+            store.dispatch(getGameActionPending());
+            GameApi.getGame(gameId).then((game: Game) => {
+                store.dispatch(getGameActionSuccess(game));
+
+                store.dispatch(getGameUsersActionPending(users));
+                UserApi.getUsers(game.players.map(p => p.userId)).then((resp: GetUsersResponse) => {
+                    store.dispatch(getGameUsersActionSuccess(resp.users));
+                }).catch((error: AxiosError) => store.dispatch(getGameUsersActionFailed(error.message)));
+            }).catch((error: AxiosError) => store.dispatch(getGameActionFailed(error.message)));
+        }
     }
 
-    const accessToken = getAccessToken();
-    const stompClient = Stomp.client(`${webSocketUrl}?token=${accessToken}`);
-    stompClient.connect({"X-Authorization": accessToken}, () => {
-        stompClient.subscribe(`/topic/game/${gameId}`, function (msg) {
-            const gameEvent = JSON.parse(msg.body)
-            if (gameEvent.type == "GAME_UPDATED") {
-                store.dispatch(getGameActionPending());
-                GameApi.getGame(gameId).then((game: Game) => {
-                    store.dispatch(getGameActionSuccess(game));
-
-                    store.dispatch(getGameUsersActionPending(users));
-                    UserApi.getUsers(game.players.map(p => p.userId)).then((resp: GetUsersResponse) => {
-                        store.dispatch(getGameUsersActionSuccess(resp.users));
-                    }).catch((error: AxiosError) => store.dispatch(getGameUsersActionFailed(error.message)));
-                }).catch((error: AxiosError) => store.dispatch(getGameActionFailed(error.message)));
+    const requester = (reactiveSocket: ReactiveSocket<any, any>) => {
+        reactiveSocket.requestStream({
+            metadata: encodeCompositeMetadata([
+                [MESSAGE_RSOCKET_ROUTING, encodeRoute(route)]
+            ])
+        }).subscribe({
+            onError: errorHandler,
+            onNext: responseHandler,
+            onSubscribe: (subscription: ISubscription) => {
+                console.log("RSocket subscribed.");
+                subscription.request(MAX_STREAM_ID);
+            },
+            onComplete: () => {
+                console.log("RSocket subscription complete.")
             }
-        });
+        })
+    }
 
-        stompClient.reconnect_delay = 1000
-    })
-
-    return stompClient
+    client.connect().then(reactiveSocket => {
+        requester(reactiveSocket);
+    }, errorHandler);
 }
 
 export default webSocketMiddleware;
